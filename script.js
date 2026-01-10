86e13946d7289975557f6b6b687f38780a2c9a8b
Dana <dkbuchshtav@gmail.com>
2026-01-10 08:38:26 +0100

    added pattern generation (isn't correct yet)

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ TODO:
         allow lines which make up multiple stitches - a la 2 down, 2 left
     fix bug when user changes viewport size without moving their mouse... actually does that matter. like at all
     dynamic board size!
+    dynamic pattern name
+    dynamic colors
 
 
 
@@ -23,14 +25,255 @@ DONE:
 */  
 
 
+// export button functionality
 const btnExport = document.getElementById("exportBtn");
 btnExport.addEventListener('click', onExportClick);
 
+class Stitch {
+    static nextId = 1;
+
+    // x1 is the leftmost x, y1 is the uppermost y
+    constructor(x1, y1, x2, y2) {
+        this.id = Stitch.nextId++;
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    static compare(a, b) {
+        var ret = a.y1 - b.y1;
+        if (ret == 0) {
+            ret = a.x1 - b.x1;
+        }
+        return ret;
+    }
+
+    is_horizontal() {
+        return this.y1 == this.y2;
+    }
+
+    is_vertical() {
+        return this.x1 == this.x2;
+    }
+
+    is_diagonal() {
+        return !this.is_horizontal() && !this.is_vertical();
+    }
+
+    get_A_stitch() {
+        if (this.is_horizontal()) {
+            return "F";
+        } else {
+            return "B";
+        }
+    }
+
+    static get_default_A_stitch() {
+        return "B";
+    }
+
+    get_B_stitch() {
+        if (this.is_vertical()) {
+            return "B";
+        }
+        else {
+            return "F";
+        }
+    }
+
+    static get_default_B_stitch() {
+        return "F";
+    }
+}
+
+class Pattern {
+    constructor(height, width, stitches) {
+        this.rowsNumA = height;
+        this.rowsNumB = height - 1;
+        this.rowsA = [];
+        this.rowsB = [];
+        this.columnsNumA = width;
+        this.columnsNumB = width - 1;
+
+        this.stitches = stitches; // array of Stitch objects
+    }
+
+    parse() {
+        // fill rowsA, rowsB, columnsA, columnsB based on this.stitches
+        var currStitchIndex = 0;
+
+        if (this.stitches.length == 0) {
+            // fill with default stitches
+            for (let i=0; i<this.rowsNumA; i++) {
+                this.rowsA.push(new Array(this.columnsNumA).fill(Stitch.get_default_A_stitch()));
+            }
+            for (let i=0; i<this.rowsNumB; i++) {
+                this.rowsB.push(new Array(this.columnsNumB).fill(Stitch.get_default_B_stitch()));
+            }
+            return;
+        }
+
+        var currStitch = this.stitches[currStitchIndex];
+        for (let i = 0; i < this.rowsNumA; i++) {
+            if (i < currStitch.y1) {
+                this.rowsA.push(new Array(this.columnsNumA).fill(Stitch.get_default_A_stitch()));
+            }
+            else 
+            {
+                let row = new Array(currStitch.x1 - 1).fill(Stitch.get_default_A_stitch());
+                    
+                while (i == currStitch.y1) {
+
+                    row.push(currStitch.get_A_stitch());
+                    var lastX = currStitch.x1;
+                    currStitchIndex++;
+                    if (currStitchIndex >= this.stitches.length) {
+                        break;
+                    }
+                    currStitch = this.stitches[currStitchIndex];
+                    // fill in any gaps with default stitches
+                    var gapSize = currStitch.x1 - lastX - 1;
+
+                    for (let j=0; j<gapSize; j++) {
+                        row.push(Stitch.get_default_A_stitch());
+                    }
+
+                }
+
+                // fill in any remaining spaces in the row with default stitches
+                for (let j=row.length; j<this.columnsNumA; j++) {
+                    row.push(Stitch.get_default_A_stitch());
+                }
+
+                this.rowsA.push(row);
+            }
+        }
+
+        currStitchIndex = 0;
+        currStitch = this.stitches[currStitchIndex];
+
+        for (let i = 0; i < this.rowsNumB; i++) {
+            if (i < currStitch.y1) {
+                this.rowsB.push(new Array(this.columnsNumB).fill(Stitch.get_default_B_stitch()));
+            }
+            else 
+            {
+                let row = new Array(currStitch.x1 - 1).fill(Stitch.get_default_B_stitch());
+                while (i == currStitch.y1) {
+                    row.push(currStitch.get_B_stitch());
+                    var lastX = currStitch.x1;
+
+                    currStitchIndex++;
+                    if (currStitchIndex >= this.stitches.length) {
+                        break;
+                    }
+                    currStitch = this.stitches[currStitchIndex];
+                    // fill in any gaps with default stitches
+                    var gapSize = currStitch.x1 - lastX - 1;
+                    for (let j=0; j<gapSize; j++) {
+                        row.push(Stitch.get_default_B_stitch());
+                    }
+                }
+
+                // fill in any remaining spaces in the row with default stitches
+                for (let j=row.length; j<this.columnsNumB; j++) {
+                    row.push(Stitch.get_default_B_stitch());
+                }
+                this.rowsB.push(row);
+            }
+
+        }
+    }
+
+    compress_row(row) {
+        var new_row = "";
+        var currentStitchVal = row[0];
+        var currentStitchCount = 1;
+
+        for (let i=1; i<row.length; i++) {
+            if (row[i] == currentStitchVal) {
+                currentStitchCount++;
+            } else {
+                // append to new_row
+                if (currentStitchCount > 1) {
+                    new_row += currentStitchVal + currentStitchCount.toString();
+                } else {
+                    new_row += currentStitchVal;
+                }
+                // reset counters
+                currentStitchVal = row[i];
+                currentStitchCount = 1;
+            }
+        }
+
+        // append final stitch
+        if (currentStitchCount > 1) {
+            new_row += currentStitchVal + currentStitchCount.toString();
+        } else {
+            new_row += currentStitchVal;
+        }
+
+        return new_row;
+    }
+
+    compress() {
+        // reduce multiple stitches in a row/column into a single representation
+        for (let i=0; i<this.rowsA.length; i++) {
+            this.rowsA[i] = this.compress_row(this.rowsA[i]);
+        }
+        for (let i=0; i<this.rowsB.length; i++) {
+            this.rowsB[i] = this.compress_row(this.rowsB[i]);
+        }
+    }
+
+    toString() {
+        var output = "";
+        for (let i=0; i<this.columnsNumB; i++) {
+            output += (i + 1).toString() + "A: ";
+            output += this.rowsA[i] + "\n";
+            output += (i + 1).toString() + "B: ";
+            output += this.rowsB[i] + "\n";
+        }
+
+        output += (this.columnsNumB + 1).toString() + "A: ";
+        output += this.rowsA[this.columnsNumB] + "\n";
+
+        return output;
+    }
+}
+
+var stitches = [];
+
+
+
+
+function createPattern() {
+    stitches.sort(Stitch.compare);
+    var pattern = new Pattern(height, width, stitches);
+    pattern.parse();
+    pattern.compress();
+    return pattern;
+}
+
 function onExportClick(event) {
-    let output = "";
+    filename = "pattern.txt";
 
-    
+    let pattern = createPattern();
+    let output = pattern.toString();
 
+    const blob = new Blob([output], {type: 'text'});
+    if(window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(blob, filename);
+    }
+    else{
+        const elem = window.document.createElement('a');
+        elem.href = window.URL.createObjectURL(blob);
+        elem.download = filename;        
+        document.body.appendChild(elem);
+        elem.click();        
+        document.body.removeChild(elem);
+    }
 }
 
 
@@ -225,7 +468,6 @@ canvas.addEventListener('click', function(evt) {
                 }
             }
 
-
             // toggle the line
             // if the line already exists, find it
             const indexOfExisting = lines.findIndex(item => 
@@ -236,9 +478,15 @@ canvas.addEventListener('click', function(evt) {
             // if you didn't find it, add it
             if (indexOfExisting == -1) {
                 lines.push(structuredClone(placing));
+                stitches.push(new Stitch(placing.x1 - 1, placing.y1 - 1, placing.x2 - 1, placing.y2 - 1));
             // if you did find it, remove it
             } else {
                 lines.splice(indexOfExisting, 1);
+                stitches = stitches.filter(item =>
+                    !(item.x1 == placing.x1 - 1 && 
+                    item.y1 == placing.y1 - 1 && 
+                    item.x2 == placing.x2 - 1 && 
+                    item.y2 == placing.y2 - 1));
             }
 
 
